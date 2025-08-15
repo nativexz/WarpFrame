b1b8dfdb14dca11fc2062e9d1fb4d70ed6315929
@native <bulatemil@gmail.com>
2025-08-15 13:46:25 +0300

    Update Controller.lua
    Example Script.

--- src/StarterGui/WarpFrameUI/Controller.lua
+++ src/StarterGui/WarpFrameUI/Controller.lua
@@ -1,4 +1,6 @@
-local WarpFrame = require(game.ReplicatedStorage.WarpFrame)
+-- THIS IS A EXAMPLE.... btw
+
+local Renderer = require(game.ReplicatedStorage.WrapFrame)
 local imageLabel = script.Parent.ImageContainer
 local staticButton = script.Parent.StaticButton
 local gifButton = script.Parent.GifButton
@@ -7,12 +9,12 @@ imageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
 imageLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
 imageLabel.BackgroundTransparency = 1
 
-local renderer = WarpFrame.new(imageLabel)
+local renderer = Renderer.new(imageLabel)
 
 staticButton.MouseButton1Click:Connect(function()
-	task.spawn(renderer.request, renderer, {type = "sfw"})
+	task.spawn(renderer.rq, renderer, {type = "sfw"})
 end)
 
 gifButton.MouseButton1Click:Connect(function()
-	task.spawn(renderer.request, renderer, {type = "gif"})
+	task.spawn(renderer.rq, renderer, {type = "gif"})
 end)
